a925b60c5a1e1e177b0c78af53103cd476a5ddaf
Keller Zhang <keller_123@hotmail.com>
2018-08-01 17:52:58 +0800

    submitted by Keller

---
+++ config.py
@@ -0,0 +1,14 @@
+MONGO_URL = 'localhost'
+MONGO_PORT = 27017
+MONGO_DB = 'taobao'
+MONGO_COLLECTION = 'products'
+
+KEYWORD = 'ipad'
+
+MAX_PAGE = 100
+
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
+
+HTTP_PROXY='127.0.0.1:59798'
+SOCKS_PROXY='127.0.0.1:59802'
+TIMEOUT=10
